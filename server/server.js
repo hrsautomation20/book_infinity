16c2733077c125907bb197a13080b99c3cf90c76
Hardik Rola <hrsautomation20@gmail.com>
2022-01-09 15:18:42 -0500

    Added resource files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
+// import the path module
 const path = require('path');
+const express = require('express');
+// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
+// middle function for authentication
 const { authMiddleware } = require('./utils/auth');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -18,7 +24,6 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Static
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -29,7 +34,7 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!!`);
+    console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
